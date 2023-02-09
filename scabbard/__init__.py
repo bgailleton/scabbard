d0b380423da74086ba0a726b83a7c066be2c0281
bgailleton <boris.gailleton@gmail.com>
2023-02-09 18:35:37 +0100

    initial commit

---
+++ scabbard/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for scabbard."""
+
+__author__ = """Boris Gailleton"""
+__email__ = 'boris.gailleton@univ-rennes1.fr'
+__version__ = '0.0.1'
+
+from .enumeration import *
+from .io import *
+from .fastflood import *
