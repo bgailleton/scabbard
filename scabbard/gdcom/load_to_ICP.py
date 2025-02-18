381b757a371c6f368563fba420999eb3efa886ed
bgailleton <boris.gailleton@gmail.com>
2025-02-18 21:21:24 +0100

    COmmenting and fixing gdcom

--- scabbard/gdcom/load_to_ICP.py
+++ scabbard/gdcom/load_to_ICP.py
@@ -1,8 +1,18 @@
 import scabbard as scb
 import numpy as np
 import click
+'''
+Manage the transfer of data from scabbard-python to IPC (Inter-Processes Communations)
+Warning: stores data on the RAM directly, very prone to memory leak if badly manage
+So far Only work on Linux (and Mac?). Will port it on Windows as soon as API gets stable.
+
+Authors: B.G.
+'''
+# Json for writing dictionary-like data
 import json
+# Memory maps for writing in the memory
 import mmap
+# Struct helps formatting 
 import struct
 
 
@@ -10,7 +20,18 @@ import struct
 @click.argument('fname', type = str)
 @click.argument('prefix', type = str)
 def simple_load(fname, prefix):
+	'''
+	Load a single grid to the Memory map from a raster file
+	Metadata as a json -> {prefix}_meta
+	grid as binary data -> {prefix}_array
 
+	Arguments:
+		- fname (str) : the file to load
+		- prefix (str): the prefix for file
+
+	Returns:
+		- Nothing, save a json with meta data and the grid array
+	'''
 	grid = scb.io.load_raster(fname)
 
 	# Example data to save
@@ -21,9 +42,12 @@ def simple_load(fname, prefix):
 		"xmin": grid.geo.xmin,
 		"ymin": grid.geo.ymin,
 		"dtype": str(grid.Z.dtype),
-		"fname": prefix + "_array"
+		"fname": prefix + "_array",
+		"zmin": float(np.nanmin(grid.Z)),
+		"zmax": float(np.nanmax(grid.Z)),
 	}
 
+	print(metadata)
 	# Save the JSON file to /dev/shm/
 	with open("/dev/shm/"+prefix+"_meta", 'w+') as f:
 		json.dump(metadata, f)
